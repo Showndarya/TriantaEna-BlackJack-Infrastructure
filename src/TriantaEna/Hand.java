b8c7f40dcd0fdd9026861fe0e1680cdcd381d5e6
Showndarya Madhavan <hansolo9821@gmail.com>
2022-10-11 14:58:38 -0400

    Initial Implementation - Hand class

--- src/TriantaEna/Hand.java
+++ src/TriantaEna/Hand.java
@@ -1,5 +1,33 @@
 package TriantaEna;
 
-public class Hand {
+import java.util.ArrayList;
 
+import GameObjects.Card;
+
+public class Hand {
+	
+	private int CurrentValue;
+	private ArrayList<Card> CurrentCards;
+	
+	public Hand() 
+	{
+		this.CurrentCards=new ArrayList<Card>();
+		this.CurrentValue=0;
+	}
+	
+	public ArrayList<Card> GetCurrentCards(){
+		return this.CurrentCards;
+	}
+	
+	public int GetCurrentValue(){
+		return this.CurrentValue;
+	}
+	
+	public void AddValue(int value) {
+		this.CurrentValue+=value;
+	}
+	
+	public void AddCard(Card card) {
+		this.CurrentCards.add(card);
+	}
 }
