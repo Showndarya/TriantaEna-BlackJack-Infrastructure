5cd49c5641d0b640791687f3cbce948be7f981aa
Prithviraj Khelkar <pkhelkar26@gmail.com>
2022-10-12 14:31:00 -0400

    Bet move done

--- src/TriantaEna/BetMove.java
+++ src/TriantaEna/BetMove.java
@@ -4,7 +4,7 @@ import GameObjects.Move;
 import Common.Tuple;
 
 public class BetMove implements Move{
-	public Boolean IsValidMove(Tuple move, Board b) {
+	public Boolean isValidMove(Tuple move, GameObjects.Board board) {
         /* 
             Validator for the move
         */
@@ -17,8 +17,22 @@ public class BetMove implements Move{
         return true;
     }
 
-    public void MakeMove(Tuple move, Board b) {
+    public void makeMove(Tuple move, GameObjects.Board board) {
         Player player = (Player) move.GetFirst();
+        int bet = (int) move.GetSecond();
+
+        int betLimit = player.getBetLimit();
+        player.setBetLimit(betLimit - bet);
+
         player.AddMove(move);
     }
+
+    public Boolean isWinningMove(Tuple move, GameObjects.Board board, int pieceId) {
+        return null;
+    }
+
+    public Boolean isValidMove(Tuple move, int boardLength, int boardWidth, GameObjects.Board board) {
+        return null;
+    }
+
 }
